eb8b10eb966636a3c3426d65096f61e9d0b9317a
Haryandi Andi H'Seven <38398405+haryandi@users.noreply.github.com>
2023-04-06 09:26:55 +0700

    Create config.py

---
+++ Uploader/config.py
@@ -0,0 +1,28 @@
+# sample config file 
+
+class Config(object):
+
+    # get a token from @BotFather
+    BOT_TOKEN = "6074774437:AAEhZI0svuMjNXIc_bj-jZUHaISB8sQWgtE"
+    
+    # Get these values from my.telegram.org
+    API_ID = 27386985
+    API_HASH = "337303f9e609e0b9ad977b776ae6c4ca"
+    
+    # No need to change
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    ADL_BOT_RQ = {}
+    CHUNK_SIZE = 128
+    TG_MAX_FILE_SIZE = 4194304000
+    HTTP_PROXY = ""
+    PROCESS_MAX_TIMEOUT = 3700
+    
+    # TG Ids
+    LOG_CHANNEL = -1001798969594
+    OWNER_ID = 5537296426
+    
+    # bot username without @
+    BOT_USERNAME = "kirimfile_bot"
+    
+    # auth users
+    AUTH_USERS = [OWNER_ID, 969343803]
